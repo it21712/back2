7c2ba4cdffe32caee9c7684bde1ac03fb2cc8854
convr <it21712@hua.gr>
2022-02-21 20:04:05 +0200

    first commit

---
+++ src/main/java/gr/ds/restapi/entity/CivicOfficial.java
@@ -0,0 +1,40 @@
+package gr.ds.restapi.entity;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+
+@Entity
+@Table(name = "civic_official")
+@PrimaryKeyJoinColumn(name = "id")
+public class CivicOfficial extends User implements Serializable {
+
+    @Column(name="code")
+    private int code;
+
+    public CivicOfficial(){}
+
+
+
+    public CivicOfficial(int id, String username, String passcode, String fullName, String region, boolean enabled) {
+        setId(id);
+        setUsername(username);
+        setPasscode(passcode);
+        setFullName(fullName);
+        setRegion(region);
+        setEnabled(enabled);
+    }
+
+    public CivicOfficial(String fullName, String region){
+        this.setFullName(fullName);
+        this.setRegion(region);
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+}
---
+++ src/main/java/gr/ds/restapi/entity/Vet.java
@@ -0,0 +1,40 @@
+package gr.ds.restapi.entity;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+
+@Entity
+@PrimaryKeyJoinColumn(name = "id")
+public class Vet  extends User implements Serializable {
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name="code")
+    private int code;
+
+    public Vet(){}
+
+    public Vet(String name, int code) {
+
+        this.name = name;
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+}
